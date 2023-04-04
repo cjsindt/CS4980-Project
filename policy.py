3cb818f139d80a62cb2ee752725dc8416fe27001
Connor Sindt <connor-sindt@uiowa.edu>
2023-04-04 13:50:08 -0500

    20-23 school closure

--- policy.py
+++ policy.py
@@ -1,21 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# fills 3 variables (data2020, data2021, data2022) with the raw USA csv data from 2020-2022
+# fills 4 variables (data2020, data2021, data2022, all_data) with the raw USA csv data from 2020-2022
 def read_data():
-    global data2020, data2021, data2022
+    global data2020, data2021, data2022, all_data
     data2020 = pd.read_csv('./data/United States/OxCGRT_USA_differentiated_withnotes_2020.csv', dtype=str)
     data2021 = pd.read_csv('./data/United States/OxCGRT_USA_differentiated_withnotes_2021.csv', dtype=str)
     data2022 = pd.read_csv('./data/United States/OxCGRT_USA_differentiated_withnotes_2022.csv', dtype=str)
+    all_data = pd.concat([data2020, data2021, data2022])
 
+def nat_total_school_closing():
+    school_closing = all_data[all_data['Jurisdiction'] == 'NAT_TOTAL']
+    ticks = ['No Closure', 'Recommended Closure', 'Required Closure for Some', 'Required Closure for All']
+
+    plt.plot(range(len(school_closing)), school_closing['C1E_School closing'].astype(float), 'r', label='Everyone')
+    plt.plot(range(len(school_closing)), school_closing['C1NV_School closing'].astype(float), 'b', label='Non-Vaccinated')
+    plt.plot(range(len(school_closing)), school_closing['C1V_School closing'].astype(float), 'g', label='Vaccinated')
+    plt.xlabel('Day #')
+    plt.yticks([0,1,2,3], ticks)
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
     read_data()
-    school_closing = data2020[data2020['Jurisdiction'] == 'NAT_TOTAL']
-    #schools = data['C1E_School closing'].astype(float)
-    print(len(data2020))
-    print(len(school_closing))
-    plt.plot(range(len(school_closing)), school_closing['C1E_School closing'])
-    plt.xlabel('Day #')
-    plt.ylabel('School closing severity')
-    plt.show()
+    nat_total_school_closing()
