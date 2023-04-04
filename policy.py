0f473cbe4d9650dc72b228ec4fc4bac2dac0e958
Connor Sindt <connor-sindt@uiowa.edu>
2023-04-04 13:11:07 -0500

    school closings

--- policy.py
+++ policy.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    data = pd.read_csv('./covid-policy-tracker/data/United States/OxCGRT_USA_differentiated_withnotes_2020.csv')
-    
+    data = pd.read_csv('./data/United States/OxCGRT_USA_differentiated_withnotes_2020.csv', dtype=str)
+    school_closing = data[data['C1E_School closing'].astype(float) > 0]
+    print(len(data))
+    print(len(school_closing))
