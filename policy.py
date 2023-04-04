77586cb162582fec8aa04abc00b7f33de0160f60
Connor Sindt <connor-sindt@uiowa.edu>
2023-04-04 13:20:09 -0500

    national school closings

--- policy.py
+++ policy.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     data = pd.read_csv('./data/United States/OxCGRT_USA_differentiated_withnotes_2020.csv', dtype=str)
-    school_closing = data[data['C1E_School closing'].astype(float) > 0]
+    school_closing = data[data['Jurisdiction'] == 'NAT_TOTAL']
+
+    #schools = data['C1E_School closing'].astype(float)
     print(len(data))
     print(len(school_closing))
+    plt.plot(range(len(school_closing)), school_closing['C1E_School closing'])
+    plt.xlabel('Day #')
+    plt.ylabel('School closing severity')
+    plt.show()
