11e10cf851476f09fbd529e4649d9db7cbabddca
Connor Sindt <connor-sindt@uiowa.edu>
2023-04-04 13:28:00 -0500

    read_data()

--- policy.py
+++ policy.py
@@ -1,12 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    data = pd.read_csv('./data/United States/OxCGRT_USA_differentiated_withnotes_2020.csv', dtype=str)
-    school_closing = data[data['Jurisdiction'] == 'NAT_TOTAL']
+# fills 3 variables (data2020, data2021, data2022) with the raw USA csv data from 2020-2022
+def read_data():
+    global data2020, data2021, data2022
+    data2020 = pd.read_csv('./data/United States/OxCGRT_USA_differentiated_withnotes_2020.csv', dtype=str)
+    data2021 = pd.read_csv('./data/United States/OxCGRT_USA_differentiated_withnotes_2021.csv', dtype=str)
+    data2022 = pd.read_csv('./data/United States/OxCGRT_USA_differentiated_withnotes_2022.csv', dtype=str)
 
+
+if __name__ == '__main__':
+    read_data()
+    school_closing = data2020[data2020['Jurisdiction'] == 'NAT_TOTAL']
     #schools = data['C1E_School closing'].astype(float)
-    print(len(data))
+    print(len(data2020))
     print(len(school_closing))
     plt.plot(range(len(school_closing)), school_closing['C1E_School closing'])
     plt.xlabel('Day #')
