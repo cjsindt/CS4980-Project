5cf4ca785d2e2ddaa3a7863e044ccd15f6a34db6
Connor Sindt <connor-sindt@uiowa.edu>
2023-04-24 12:24:11 -0500

    removed strictly new york and colorado data and replaced it with data from all states

--- src/correlation.py
+++ src/correlation.py
@@ -4,12 +4,21 @@ from scipy.stats import pearsonr
 
 
 def read_data():
-    global policydata2020, policydata2021, policydata2022, all_policydata
+    global policydata2020, policydata2021, policydata2022, all_policydata, casedata
     policydata2020 = pd.read_csv('./data/United States/OxCGRT_USA_differentiated_withnotes_2020.csv', dtype=str)
     policydata2021 = pd.read_csv('./data/United States/OxCGRT_USA_differentiated_withnotes_2021.csv', dtype=str)
     policydata2022 = pd.read_csv('./data/United States/OxCGRT_USA_differentiated_withnotes_2022.csv', dtype=str)
     all_policydata = pd.concat([policydata2020, policydata2021, policydata2022])
 
+    casedata = pd.read_csv('./data/ILINet.csv')
+
+
+
+
+def correlate():
+    pass
 
 if __name__ == '__main__':
     read_data()
+
+    
