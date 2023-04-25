e44f280c653b76ba5938570cafd316db8c8bf55a
Connor Sindt <connor-sindt@uiowa.edu>
2023-04-25 14:22:18 -0500

    aggregate case data into epiweek averages

--- src/flu_stats.py
+++ src/flu_stats.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def read_data():
@@ -7,15 +8,28 @@ def read_data():
 
 
 # takes a list of years and an optional state to calcluate the average peaks from the given years flu seasons
-def avg_peak(years=[2020, 2021, 2022], state='')
+def avg_peak(years=[2020, 2021, 2022], state=''):
     peaks = []
     for y in years:
         if state == '':
-            cases = casedata.query(f'')
+            cases = casedata.query(f'(YEAR == {y-1} & WEEK >= 40) | (YEAR == {y} & WEEK <= 20)')
+            cases = aggregate_cases(cases)
         else:
-            cases = casedata.query(f'')
+            cases = casedata.query(f'((YEAR == {y-1} & WEEK >= 40) | (YEAR == {y} & WEEK <= 20)) & REGION == \"{state}\"')
+            cases = aggregate_cases(cases)
+
+# helper function for avg_peak()
+# takes case data from query in avg_peak and aggregates it into epiweeks
+# if there are multiple regions specified, averages over all regions
+def aggregate_cases(cases):
+    func = lambda x: x + 12 if x <= 20 else x - 40
+    cases = cases[['WEEK', '%UNWEIGHTED ILI']].replace('X', 0).astype(float).groupby(['WEEK'], as_index=False).mean()
+    cases['WEEK'] = cases['WEEK'].apply(func)
+    cases = cases.sort_values(by=['WEEK'])
+    return cases
 
 
 if __name__ == '__main__':
     read_data()
 
+    avg_peak(state='New York')
